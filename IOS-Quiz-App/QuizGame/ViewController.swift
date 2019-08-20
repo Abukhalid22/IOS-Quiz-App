9221e14c482d9ff98413b7fd2d0e8e81f40cb83e
Abdinasir Hussein <husseinfinland94@hotmail.com>
2019-08-20 02:46:13 +0100

    New Commit

---
+++ IOS-Quiz-App/QuizGame/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  QuizGame
+//
+//  Created by Abdinasir Hussein on 25/02/2018.
+//  Copyright © 2018 Abdinasir Hussein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var window: UIWindow?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title="Quiz Game"
+        self.view.backgroundColor=UIColor.white
+        
+        setupViews()
+    }
+    
+    @objc func btnGetStartedAction() {
+        let v=QuizVC()
+        self.navigationController?.pushViewController(v, animated: true)
+    }
+    
+    func setupViews() {
+        self.view.addSubview(lblTitle)
+        lblTitle.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 150).isActive=true
+        lblTitle.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive=true
+        lblTitle.widthAnchor.constraint(equalToConstant: 250).isActive=true
+        lblTitle.heightAnchor.constraint(equalToConstant: 80).isActive=true
+        
+        self.view.addSubview(btnGetStarted)
+        btnGetStarted.heightAnchor.constraint(equalToConstant: 50).isActive=true
+        btnGetStarted.widthAnchor.constraint(equalToConstant: 150).isActive=true
+        btnGetStarted.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive=true
+        btnGetStarted.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 0).isActive=true
+    }
+    
+    let lblTitle: UILabel = {
+        let lbl=UILabel()
+        lbl.text="Quiz App"
+        lbl.textColor=UIColor.darkGray
+        lbl.textAlignment = .center
+        lbl.font = UIFont.systemFont(ofSize: 46)
+        lbl.numberOfLines=2
+        lbl.translatesAutoresizingMaskIntoConstraints=false
+        return lbl
+    }()
+    
+    let btnGetStarted: UIButton = {
+        let btn=UIButton()
+        btn.setTitle("Get Started", for: .normal)
+        btn.setTitleColor(UIColor.white, for: .normal)
+        btn.backgroundColor=UIColor.orange
+        btn.layer.cornerRadius=5
+        btn.layer.masksToBounds=true
+        btn.translatesAutoresizingMaskIntoConstraints=false
+        btn.addTarget(self, action: #selector(btnGetStartedAction), for: .touchUpInside)
+        return btn
+    }()
+}
